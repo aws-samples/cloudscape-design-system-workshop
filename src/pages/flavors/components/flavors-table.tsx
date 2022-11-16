dbbc30e612fbd4ce5924d09640350cc04db000b5
Johannes Weber <jowejowe@amazon.com>
2022-11-16 15:03:04 +0100

    chore: Set jsx to "react" instead of "react-jsx" and import react in tsx files
    Some IDEs do not support the "react-jsx" setting and highlight
    issues when using components. This change helps to mitigate that.

--- src/pages/flavors/components/flavors-table.tsx
+++ src/pages/flavors/components/flavors-table.tsx
@@ -1,5 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
+import React from 'react';
 import { ReactNode, useState } from 'react';
 
 import { useCollection } from '@cloudscape-design/collection-hooks';
